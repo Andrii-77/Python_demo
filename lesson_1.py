aa47f1cfb750eb5d3dd54c5d7323f7a7223d63f0
Andrii-77 <andrijsmaga@gmail.com>
2025-03-01 13:20:07 +0200

    First commit and push.

---
+++ lesson_1.py
@@ -0,0 +1,151 @@
+# py 01 ins & create
+# print("Hello World!!!")
+
+print('------------------')
+
+# py 02 variables
+# f = 5.999
+# i = int(f)
+# print(i)
+#
+# i = 5
+# f = float(i)
+# print(f)
+# print(type(f))
+#
+# i = 5
+# s = str(i)
+# print(s)
+# print(type(s))
+#
+# i = 5
+# b = bool(i)
+# print(b)
+# print(type(b))
+#
+# i = 0
+# b = bool(i)
+# print(b)
+# print(type(b))
+
+print('------------------')
+
+# py 03 operators
+# i = 6 + 5
+# print(i)
+#
+# i = 30 / 5
+# print(i)
+#
+# i = 30 // 5
+# print(i)
+#
+# i = 2 ** 3
+# print(i)
+#
+# i = 11 % 3
+# print(i)
+#
+# i = 15 % 4
+# print(i)
+#
+# i = 15 % 5
+# print(i)
+#
+# print(5 < 6)
+#
+# print(5 > 6)
+#
+# print(5 != 6)
+#
+# i = 2
+# i += 1
+
+print('------------------')
+
+# py 04 if else
+# if 5 < 6:
+#     print('ok')
+#
+# if 5 == 6:
+#     print('ok')
+# else:
+#     print('error')
+#
+# i=5
+# if i <= 6:
+#     print('ok')
+# else:
+#     print('error')
+#
+# i=5
+# if i==4:
+#     print('i=4')
+# elif i==5:
+#     print('i=5')
+# elif i==6:
+#     print('i=6')
+# else:
+#     print('error')
+#
+# i=6
+# if i==5 or i==6:
+#     print('ok')
+#
+# i=6
+# if i==5 and i==6:
+#     print('ok')
+#
+# i=False
+# if not i:
+#     print('good')
+
+print('------------------')
+
+# py 05 while
+# i = 0
+# while i < 5:
+#     print(i)
+#     i += 1
+# else:
+#     print('ok')
+#
+# i = 6
+# while i < 5:
+#     print(i)
+#     i += 1
+# else:
+#     print('the end')
+
+print('------------------')
+
+# # py 06 for
+# for i in range(10):
+#     print(i)
+#
+# for i in range(6, 10):
+#     print(i)
+#
+# for i in range(0, 20, 5):
+#     print(i)
+
+print('------------------')
+
+# py 07 continue, break
+for i in range(500):
+    if i==5:
+        break
+    print(i)
+else:
+    print('the end')
+print('some')
+
+for i in range(10):
+    if i==5 or i==3 or i==7:
+        continue
+    print(i)
+else:
+    print('the end')
+print('some')
+
+print('------------------')
